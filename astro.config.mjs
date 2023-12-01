1858694e444e4dccb99b93c22c3e6a5ab764fd3e
Julien Fayard <julien@huitquatre.fr>
2023-12-01 15:40:44 +0100

    change Astro.site to netlify template

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.monsite.fr",
+  site: "https://template-prof-en-redac.netlify.app/",
   integrations: [tailwind(), sitemap()],
 });
