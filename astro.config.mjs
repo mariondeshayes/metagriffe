f09d403d7e536baf42ca0f5d50016ae43478deb8
Julien Fayard <julien@huitquatre.fr>
2024-01-18 16:18:04 +0100

    marion

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://template-prof-en-redac.netlify.app/",
+  site: "https://metagriffe.fr/",
   integrations: [tailwind(), sitemap()],
 });
